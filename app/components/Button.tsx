99b032c756f4b35969dd40e9ad5168dc00459a01
Abosi-Godwin <bbnl6060@github.com>
2025-10-24 10:20:01 +0100

    updated

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 interface ButtonProps {
   text: string;
   style: string | null;
@@ -5,8 +7,10 @@ interface ButtonProps {
 }
 const defaultStyle =
   "px-3 py-1.5 rounded-md text-white font-bold text-center bg-sapphireSurge outline-none border-0 w-fit";
-const Button = ({ text, style }: ButtonProps) => {
-  return <button className={style || defaultStyle}>Let's talk</button>;
+const Button = ({ text, style, link }: ButtonProps) => {
+  return link ?
+  <button className={style || defaultStyle}>{text || "Let's talk"}</button> :
+  <Link href="/home"/>;
 };
 
 export default Button;
